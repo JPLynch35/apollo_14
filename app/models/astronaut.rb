8be07227adb699bd923ecf00eb7e6581740914d5
JP Lynch <JPLynch35@gmail.com>
2018-07-06 19:09:01 -0600

    Add model realtionship tests for all tables and join table

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -1,5 +1,7 @@
 class Astronaut < ApplicationRecord
   validates_presence_of :name, :age, :job
+  has_many :astronaut_space_missions
+  has_many :space_missions, through: :astronaut_space_missions
 
   def self.average_age
     average(:age)
---
+++ app/models/astronaut_space_mission.rb
@@ -0,0 +1,5 @@
+class AstronautSpaceMission < ApplicationRecord
+  belongs_to :astronaut
+  belongs_to :space_mission
+
+end
